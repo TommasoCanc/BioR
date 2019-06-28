565949eb1f50425a131fc5132d0fc9885a1bf953
TommasoCanc <tommaso.cancellario@gmail.com>
2019-06-28 16:34:24 +0200

    Update Test_synoGbif.R

--- Test/Test_synoGbif.R
+++ Test/Test_synoGbif.R
@@ -2,10 +2,10 @@
 library(biomonitoR)
 
 # Function
-source("/Users/tom/Desktop/GitHub/BioR/synoGbif_BioR.R")
+source("./synoGbif_BioR.R")
 
 # load example dataset
-fish <- read.csv("/Users/tom/Desktop/GitHub/fish_prove_1.csv", sep=";")
+fish <- read.csv("./fish_prove_1.csv", sep=";")
 
 # Replace and aggregate synonyms
 rep.agr <- synoGbif(fish, end_point = nrow(fish))
