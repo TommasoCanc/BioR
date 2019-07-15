b282889216ce3950f3aca5e4e3bae36f404481a9
TommasoCanc <tommaso.cancellario@gmail.com>
2019-07-15 15:49:21 +0200

    Update Test_apiGbif.R

--- Test/Test_apiGbif.R
+++ Test/Test_apiGbif.R
@@ -5,11 +5,19 @@ library(biomonitoR)
 source("./apiGbif_BioR.R")
 
 # load example dataset
-fish <- read.csv("./fish_prove.csv", sep=";")
+fish <- read.csv("./fish_prove_1.csv", sep=";")
 
 # Create reference database from GBIF API
-refDB <- apiGbif(fish$Taxa, end_point = nrow(fish))
-refDB.bior <- refDB$taxonomy
+refDB <- apiGbif(fish$Taxa, end_point = nrow(fish), cksyno = F)
+refDB$taxonomy
+refDB$taxStat
+
+# If cksyno = T all synonyms are substituted by accepted name and all possible duplicated are 
+# aggregated.
+refDB.1 <- apiGbif(fish$Taxa, end_point = nrow(fish), cksyno = T)
+refDB.1$taxonomy
+refDB.1$taxStat
+
 
 fish.asb <- asBiomonitor(fish, dfref = refDB.bior, overwrite = T)
 fish.agR = aggregatoR(fish.asb)
